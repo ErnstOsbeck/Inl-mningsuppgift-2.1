b2f7e99b53ae9597fb4a65a4751c03ba120e96ac
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-09-15 12:26:42 +0200

    Add project files.

---
+++ Inlämningsuppgift 2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace uppgift_2._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favoribok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine(bok);
+        }
+    }
+}
